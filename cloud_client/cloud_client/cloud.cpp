1a6d50c820ef35a5edcba268d738f14cc79b4410
Austerlitz1203 <1477567612@qq.com>
2023-08-25 11:42:18 +0800

    服务端、客户端功能联合测试

--- cloud_client/cloud_client/cloud.cpp
+++ cloud_client/cloud_client/cloud.cpp
@@ -9,20 +9,6 @@ using namespace cloud;
 #define BACKUP_DIR "./backup/"
 int main()
 {
-	//FileUtil fu("./");
-	//vector<string> v;
-	//fu.ScanDirectory(&v);
-	//DataManager dm(BACKUP_FILE);
-	//for (auto &a : v)
-	//{
-	//	dm.Insert(a, "woshishiejdioa");
-	//}
-
-	//DataManager dm(BACKUP_FILE);
-	//string val;
-	//dm.GetOneByKey(".\\Util.h",&val);
-	//cout << val << endl;
-
 	cloud::BackUp bu(BACKUP_DIR, BACKUP_FILE);
 	bu.RunModule();
 	return 0;
